f49e672d5df36ebc50b2ee04196cbdd54050297d
Nick Steinhouse <nick.steinhouse@gmail.com>
2022-04-14 17:01:32 -0400

    added geocoding stuff and mapping

--- capstone/vue/src/main.js
+++ capstone/vue/src/main.js
@@ -3,10 +3,18 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCyHTg7EQ4Ujtrk_IwR43o2PdNlFHF1-MI',
+    libraries: 'places',
+  }
+});
 
 new Vue({
   router,
