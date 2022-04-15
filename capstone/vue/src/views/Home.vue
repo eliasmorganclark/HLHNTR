243a6789c1df71662c3d8d36811f7bc6f687ca0f
Dan Shaw <danielmathershaw@gmail.com>
2022-04-15 14:17:18 -0400

    Added filter functionality for the home page sidebar

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -3,58 +3,49 @@
     <!-- <h1>Home</h1>
     <p>Map placeholder</p> -->
     <div id="sidebar">
-      <h2> HELP SAVE NORTHEAST OHIO </h2>
-      <p> LIST HERE !!!!!!!!!!!!!!!!!!!!!!!!!!! </p>
-      </div>
-      <div id="mapbox">
-    <Map />
+      <h2>HELP SAVE NORTHEAST OHIO</h2>
+      <p>LIST HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!</p>
+      <hazard-list></hazard-list>
+    </div>
+    <div id="mapbox">
+      <Map />
     </div>
   </div>
 </template>
 
 <script>
-import Map from '../components/Map.vue';
+import Map from "../components/Map.vue";
+import HazardList from "@/components/HazardList.vue";
 
 export default {
-  components: { Map },
+  components: { Map, HazardList },
 
-     name: "home"
-
+  name: "home",
 };
 </script>
 
-
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,400;0,900;1,100;1,400;1,900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,400;0,900;1,100;1,400;1,900&display=swap");
 
 .home {
-  font-family: 'Kanit', sans-serif;
+  font-family: "Kanit", sans-serif;
   display: flex;
   flex-direction: row;
-;
 }
 #sidebar {
   max-width: 500px;
   border-left: 10px solid black;
   border-right: 10px solid black;
   padding: 20px;
-background-color: #999;
-color: #000;
+  background-color: #999;
+  color: #000;
 
-flex-grow: 1;
+  flex-grow: 1;
 }
 #mapbox {
-flex-grow: 5;
-
+  flex-grow: 5;
 }
 h2 {
   font-size: 30px;
-  
 }
-
-
-
-
-
-
 </style>
