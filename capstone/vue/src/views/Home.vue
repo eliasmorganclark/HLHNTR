cfe9c41edcbacb9fc558e0988b75b2121fcca5b5
Elias Clark <eliasmorganclark@gmail.com>
2022-04-14 20:08:07 -0400

    Now It's Cute: Elias did the CSS and some HTML and now it looks presentable?

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>Map placeholder</p>
+    <!-- <h1>Home</h1>
+    <p>Map placeholder</p> -->
+    <div id="sidebar">
+      <h2> HELP SAVE NORTHEAST OHIO </h2>
+      <p> LIST HERE !!!!!!!!!!!!!!!!!!!!!!!!!!! </p>
+      </div>
+      <div id="mapbox">
     <Map />
+    </div>
   </div>
 </template>
 
@@ -16,3 +22,39 @@ export default {
 
 };
 </script>
+
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,400;0,900;1,100;1,400;1,900&display=swap');
+
+.home {
+  font-family: 'Kanit', sans-serif;
+  display: flex;
+  flex-direction: row;
+;
+}
+#sidebar {
+  max-width: 500px;
+  border-left: 10px solid black;
+  border-right: 10px solid black;
+  padding: 20px;
+background-color: #999;
+color: #000;
+
+flex-grow: 1;
+}
+#mapbox {
+flex-grow: 5;
+
+}
+h2 {
+  font-size: 30px;
+  
+}
+
+
+
+
+
+
+</style>
