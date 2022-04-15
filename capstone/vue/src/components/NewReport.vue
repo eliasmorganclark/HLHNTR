ff0176d0ff9303c73f6efdbfdfceabb6475fd986
Dan Shaw <danielmathershaw@gmail.com>
2022-04-15 15:09:42 -0400

    Added State Dropdown to NewReport.vue

--- capstone/vue/src/components/NewReport.vue
+++ capstone/vue/src/components/NewReport.vue
@@ -7,7 +7,6 @@
       </h2>
     </div>
     <div class="new-report-form-container">
-
       <form class="new-report-form" v-on:submit.prevent="saveNewReport">
         <label for="hazard-type">Hazard Type</label>
         <select v-model="hazardType" name="hazard-type" id="hazard-type">
@@ -15,7 +14,6 @@
           <option value="POTHOLE">Pothole</option>
           <option value="DRAIN">Drain</option>
         </select>
-
 
         <label for="house-number">House Number</label>
         <input
@@ -35,10 +33,62 @@
         <input v-model.trim="hazard.address.city" id="city" type="text" />
 
         <label for="state">State</label>
-        <input v-model.trim="hazard.address.state" id="state" type="text" />
+        <select id="state" name="state" v-model="hazard.address.state">
+          <option value="AL">Alabama</option>
+          <option value="AK">Alaska</option>
+          <option value="AZ">Arizona</option>
+          <option value="AR">Arkansas</option>
+          <option value="CA">California</option>
+          <option value="CO">Colorado</option>
+          <option value="CT">Connecticut</option>
+          <option value="DE">Delaware</option>
+          <option value="FL">Florida</option>
+          <option value="GA">Georgia</option>
+          <option value="HI">Hawaii</option>
+          <option value="ID">Idaho</option>
+          <option value="IL">Illinois</option>
+          <option value="IN">Indiana</option>
+          <option value="IA">Iowa</option>
+          <option value="KS">Kansas</option>
+          <option value="KY">Kentucky</option>
+          <option value="LA">Louisiana</option>
+          <option value="ME">Maine</option>
+          <option value="MD">Maryland</option>
+          <option value="MA">Massachusetts</option>
+          <option value="MI">Michigan</option>
+          <option value="MN">Minnesota</option>
+          <option value="MS">Mississippi</option>
+          <option value="MO">Missouri</option>
+          <option value="MT">Montana</option>
+          <option value="NE">Nebraska</option>
+          <option value="NV">Nevada</option>
+          <option value="NH">New Hampshire</option>
+          <option value="NJ">New Jersey</option>
+          <option value="NM">New Mexico</option>
+          <option value="NY">New York</option>
+          <option value="NC">North Carolina</option>
+          <option value="ND">North Dakota</option>
+          <option value="OH">Ohio</option>
+          <option value="OK">Oklahoma</option>
+          <option value="OR">Oregon</option>
+          <option value="PA">Pennsylvania</option>
+          <option value="RI">Rhode Island</option>
+          <option value="SC">South Carolina</option>
+          <option value="SD">South Dakota</option>
+          <option value="TN">Tennessee</option>
+          <option value="TX">Texas</option>
+          <option value="UT">Utah</option>
+          <option value="VT">Vermont</option>
+          <option value="VA">Virginia</option>
+          <option value="WA">Washington</option>
+          <option value="WV">West Virginia</option>
+          <option value="WI">Wisconsin</option>
+          <option value="WY">Wyoming</option>
+        </select>
 
         <label for="zip-code">Zip Code</label>
         <input v-model.trim="hazard.address.zip" id="zip-code" type="text" />
+
         <input type="submit" value="Submit Report" />
         <input
           type="button"
@@ -76,7 +126,7 @@ export default {
         },
         repairStatus: "pending",
         reportingUser: 1,
-        severity:''
+        severity: "",
       },
     };
   },
