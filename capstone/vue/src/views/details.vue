ed458c3696e486c6e5b0495ecf9f2c7ce51ab720
Dan Shaw <danielmathershaw@gmail.com>
2022-04-19 13:51:19 -0400

    Added Permissions for viewing Editing Dashboard

--- capstone/vue/src/views/details.vue
+++ capstone/vue/src/views/details.vue
@@ -1,39 +1,38 @@
 <template>
-<div>
-  <hazard-details v-if='dataLoaded' v-bind:hazard="currentHazard" /> 
-  <manage-hazard v-if='dataLoaded' v-bind:propHazard="currentHazard" />
-</div>
+  <div v-if="dataLoaded">
+    <hazard-details v-if="dataLoaded" v-bind:hazard="currentHazard" />
+    <manage-hazard
+      v-if="this.$store.state.user.authorities[0].name == 'ROLE_ADMIN'"
+      v-bind:propHazard="currentHazard"
+    />
+  </div>
 </template>
 
 <script>
-import HazardDetails from '../components/HazardDetails.vue'
-import dataService from "@/services/DataService.js"
-import ManageHazard from '../components/ManageHazard.vue'
+import HazardDetails from "../components/HazardDetails.vue";
+import dataService from "@/services/DataService.js";
+import ManageHazard from "../components/ManageHazard.vue";
 
 export default {
   components: { HazardDetails, ManageHazard },
-  data(){
-    return{
-        dataLoaded: false,
-        currentHazard: {}
-    }
-    },
+  data() {
+    return {
+      dataLoaded: false,
+      currentHazard: {},
+    };
+  },
   mounted() {
     this.loadData();
   },
-  methods:{
-    loadData(){
+  methods: {
+    loadData() {
       dataService.getHazard(this.$route.params.hazardId).then((response) => {
         this.currentHazard = response.data;
-        this.dataLoaded=true;
-        });
-      
-    }
-    
-   },
-}
+        this.dataLoaded = true;
+      });
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
